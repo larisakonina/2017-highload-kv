c375576fba6957a72dd47a05da158f9797bcbfe8
larisakonina <larisa.dmitriewna@yandex.ru>
2018-01-01 21:42:03 +0300

    all tests passed

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,14 +14,13 @@ dependencies {
     // Annotations for better code documentation
     compile("com.intellij:annotations:12.0")
 
-    compile("org.apache.httpcomponents:fluent-hc:4.5.3")
     // JUnit test framework
     testCompile("junit:junit:4.12")
 
     // HTTP client for unit tests
-
     testCompile("org.apache.httpcomponents:fluent-hc:4.5.3")
 
+    // Guava for tests
     testCompile("com.google.guava:guava:23.1-jre")
 }
 
@@ -33,8 +32,8 @@ tasks {
 
 application {
     // Define the main class for the application
-    mainClassName = "ru.mail.polis.Server"
+    mainClassName = "ru.mail.polis.Cluster"
 
     // And limit Xmx
-    applicationDefaultJvmArgs = listOf("-Xmx1g")
+    applicationDefaultJvmArgs = listOf("-Xmx1g", "-Xverify:none")
 }
