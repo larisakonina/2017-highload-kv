c4cd6da41b0a18d1549ef7d9942975955fc8e6c1
larisakonina <larisa.dmitriewna@yandex.ru>
2018-01-01 21:11:29 +0300

    all tests passed

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,6 +19,8 @@ dependencies {
 
     // HTTP client for unit tests
     testCompile("org.apache.httpcomponents:fluent-hc:4.5.3")
+
+    testCompile("com.google.guava:guava:23.1-jre")
 }
 
 tasks {
