bb74f6fe016e29895535b833161d30ce66fa4dd8
larisakonina <larisa.dmitriewna@yandex.ru>
2018-01-01 21:31:34 +0300

    all tests passed

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,10 +14,12 @@ dependencies {
     // Annotations for better code documentation
     compile("com.intellij:annotations:12.0")
 
+    compile("org.apache.httpcomponents:fluent-hc:4.5.3")
     // JUnit test framework
     testCompile("junit:junit:4.12")
 
     // HTTP client for unit tests
+
     testCompile("org.apache.httpcomponents:fluent-hc:4.5.3")
 
     testCompile("com.google.guava:guava:23.1-jre")
